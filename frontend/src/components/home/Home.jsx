3251c9e9ba678c7b84a8e78caa362310339347f8
demetriusleonardobantim <demetriusleonardobantim98@hotmail.com>
2020-12-21 08:11:58 -0300

    crudReact

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props => 
+    <Main icon="home" title="Inicio"
+        subtitle="Projeto crud - Estudos de React">
+        <div className="display-4 title">Bem vindo!</div>    
+        <hr/>
+        <p className="mb-0">Sistema para construcao de cadastro</p>
+    </Main>
